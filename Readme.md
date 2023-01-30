019196a75fe49650710eeea982eac1ec8e94ee0a
xdpiqbx <xdpiqbx@gmail.com>
2023-01-31 00:16:25 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## [In process](https://youtu.be/KxqlJblhzfI?t=1812)
---
+++ src/main/java/com/dpiqb/security/User.java
@@ -0,0 +1,57 @@
+package com.dpiqb.security;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+  @Id
+  @GeneratedValue
+  private Integer id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+}
