f92d3a1b3bd0988a50e099610b0726a218216673
xdpiqbx <xdpiqbx@gmail.com>
2023-02-04 13:29:15 +0200

    all done

--- src/main/java/com/dpiqb/security/demo/DemoController.java
+++ src/main/java/com/dpiqb/security/demo/DemoController.java
@@ -1,6 +1,5 @@
 package com.dpiqb.security.demo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
